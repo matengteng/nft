21998232fd07fc4f669b16a0708b38f89f8b0b85
marshall <marshall@gmail.com>
2022-08-05 16:43:21 +0800

    graph

--- src/nft.ts
+++ src/nft.ts
@@ -11,66 +11,11 @@ import {
   SetUriPrefixEvent,
   Transfer
 } from "../generated/NFT/NFT"
-import { ExampleEntity } from "../generated/schema"
+
+import { TransferNFT } from "../generated/schema";
 
 export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.ClaimedWhitelist(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getClaimWhitelist(...)
-  // - contract.getCost(...)
-  // - contract.getMerkleroot(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.maxSupply(...)
-  // - contract.merkleRoot(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.paused(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
-  // - contract.uriPrefix(...)
-  // - contract.walletOfOwner(...)
+  
 }
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
@@ -91,4 +36,15 @@ export function handleSetUriMerklerootEvent(
 
 export function handleSetUriPrefixEvent(event: SetUriPrefixEvent): void {}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  let transferNFT = TransferNFT.load(event.params.from.toHex())
+  if (transferNFT == null) {
+      transferNFT = new TransferNFT(event.transaction.hash.toHex() + "_" + event.transactionLogIndex.toHex())
+      transferNFT.from = event.params.from
+      transferNFT.to = event.params.to
+      transferNFT.contract = event.transaction.to
+      transferNFT.timestamp = event.block.timestamp
+      transferNFT.tokenId = event.params.tokenId
+      transferNFT.save()
+  }
+}
